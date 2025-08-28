906b50ac35664f87271e161d7dc69dc7bb826b5c
Asilbek <asilbekeshtemirov@outlook.com>
2025-08-28 14:50:27 +0500

    .

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,7 @@
-
+export { default as ClinicCard } from "./ClinicCard";
+export { default as ClinicCarousel } from "./ClinicCarousel";
+// export { default as DoctorCard } from "./DoctorCard";
+// export { default as DoctorTypeCard } from "./DoctorTypeCard";
+// export { default as ProductCard } from "./ProductCard";
+export { default as PromotionCard } from "./PromotionCard";
+// export { default as Button } from "./Button";
