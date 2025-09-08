60066be713f4548d0aaa42e956c0081d50ecbf0d
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-09-08 19:20:42 +0300

    Doctor Type Swiper component updated

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,11 @@
 export { default as KlinikiPage } from "./Kliniki/index";
-// export { default as About } from './About';
+export { default as About } from "./About";
 // export { default as Contact } from './Contact';
 // export { default as DoctorDetail } from './DoctorDetail';
 export { default as Doctors } from "./Doctors";
-// export { default as FAQ } from './FAQ';
+export { default as FAQ } from "./FAQ";
 // export { default as Home } from './Home';
-// export { default as Kliniki } from './Kliniki';
+export { default as Kliniki } from "./Kliniki";
 // export { default as News } from './News';
 export { default as Privacy } from "./Privacy";
 export { default as Promotions } from "./Promotions";
